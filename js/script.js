8b32bb32409d3b35c22afb97a81383733dae7aca
Prakhar Pandey <prakharp4pandey@gmail.com>
2025-09-24 00:22:59 +0530

    added theme 3

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ var styleLine = document.getElementById("theme-css");
 var themeBtnBg = document.getElementById("switch-bg");
 var themeBtn1 = document.getElementById("sw-1");
 var themeBtn2 = document.getElementById("sw-2");
+var themeBtn3 = document.getElementById("sw-3");
 
 cols.forEach((c) => {
     c.addEventListener('mousedown', function () {
@@ -46,14 +47,28 @@ themeBtn1.addEventListener("click", function () {
     styleLine.href = "css/theme1.css";
     themeBtnBg.classList.add("theme1-theme-select-bg");
     themeBtnBg.classList.remove("theme2-theme-select-bg");
+    themeBtnBg.classList.remove("theme3-theme-select-bg");
     themeBtn1.style.backgroundColor = "hsl(6, 63%, 50%)";
     themeBtn2.style.backgroundColor = "#FFB3CB00";
+    themeBtn3.style.backgroundColor = "#FFB3CB00";
 });
 
 themeBtn2.addEventListener("click", function () {
     styleLine.href = "css/theme2.css";
     themeBtnBg.classList.add("theme2-theme-select-bg");
     themeBtnBg.classList.remove("theme1-theme-select-bg");
+    themeBtnBg.classList.remove("theme3-theme-select-bg");
     themeBtn2.style.backgroundColor = "hsl(25, 98%, 40%)";
     themeBtn1.style.backgroundColor = "#FFB3CB00";
+    themeBtn3.style.backgroundColor = "#FFB3CB00";
+});
+
+themeBtn3.addEventListener("click", function () {
+    styleLine.href = "css/theme3.css";
+    themeBtnBg.classList.add("theme3-theme-select-bg");
+    themeBtnBg.classList.remove("theme1-theme-select-bg");
+    themeBtnBg.classList.remove("theme2-theme-select-bg");
+    themeBtn3.style.backgroundColor = "hsl(176, 100%, 44%)";
+    themeBtn1.style.backgroundColor = "#FFB3CB00";
+    themeBtn2.style.backgroundColor = "#FFB3CB00";
 });
